64cd7b4b60a3233481f17c8090c2564632009766
saumya-datascience <81590422+saumya-datascience@users.noreply.github.com>
2021-07-17 20:50:23 -0400

    Adding the code for pouplating the drop down with samples ID/name

---
+++ Instructions/StarterCode/static/js/app.js
@@ -0,0 +1,71 @@
+/* The following is an example on how you might structure your code.
+This is not the only way to complete this assignment.
+Feel free to disregard and create your own code */
+
+// Define function that will run on page load
+function init() {
+    nameSel=d3.select("#selDataset");
+    // Read json data
+    d3.json("samples.json").then(function (response) {
+
+        console.log(response);
+        arr=response.names;
+        arr.forEach((nam) =>{
+        
+          nameSel.append("option").text(nam).property("value",nam);
+        })
+        buildMetadata(arr[0])
+        // Update charts with newly selected sample
+        buildCharts(arr[0])    
+    
+
+    });
+
+}
+
+    // Parse and filter data to get sample names
+
+    // Add dropdown option for each sample
+
+    // Call functions below using the first sample to build metadata and initial plots
+
+
+
+    // Define a function that will create metadata for given sample
+    function buildMetadata(sample) {
+
+        // Read the json data
+
+        // Parse and filter the data to get the sample's metadata
+
+        // Specify the location of the metadata and update it
+
+    }
+
+    // Define a function that will create charts for given sample
+    function buildCharts(sample) {
+
+        // Read the json data
+
+        // Parse and filter the data to get the sample's OTU data
+        // Pay attention to what data is required for each chart
+
+        // Create bar chart in correct location
+
+        // Create bubble chart in correct location
+
+    }
+
+
+    function optionChanged(sample) {
+        // The parameter being passed in this function is new sample id from dropdown menu
+        console.log(sample);
+        // Update metadata with newly selected sample
+        buildMetadata(sample)
+        // Update charts with newly selected sample
+        buildCharts(sample)
+
+    }
+
+    // Initialize dashboard on page load
+    init();
